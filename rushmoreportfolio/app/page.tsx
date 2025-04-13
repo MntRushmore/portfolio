781de58ebea6e4c1e0c04e53be4208c4895aac5f
Rushil Chopra <83889502+MntRushmore@users.noreply.github.com>
2025-04-12 19:16:03 -0700

    Delete rushmoreportfolio directory

--- rushmoreportfolio/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import BootSequence from "@/components/boot-sequence"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-black overflow-hidden font-['VT323',monospace] text-white">
-      <BootSequence />
-    </main>
-  )
-}
